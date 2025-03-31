5949568c059cb3622d88740895ccd90b65539113
VickNat <natanimashenafi20@gmail.com>
2025-03-31 12:28:15 +0300

    Initial setup

---
+++ src/app/Providers.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react';
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+const Providers: React.FC<ProvidersProps> = ({ children }) => {
+  return (
+    <>
+      {children}
+    </>
+  );
+};
+
+export default Providers;
